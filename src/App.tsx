16d95145b44622fcfdc9b44c911f01f53245898d
윤태현 <yoonth0919@gmail.com>
2024-02-20 23:51:02 +0900

    Design: 사이드바, 컨텐츠 레이아웃 설정

---
+++ src/App.tsx
@@ -0,0 +1,44 @@
+import { Outlet } from "react-router-dom";
+import { Sidebar, Header } from "@/components/layouts";
+import { useSidebarStore } from "./stores";
+import styled from "styled-components";
+
+interface SidebarContainerProps {
+  toggle: boolean;
+}
+
+function App() {
+  const { toggleSidebar } = useSidebarStore();
+
+  return (
+    <WebContainer>
+      <SidebarContainer toggle={toggleSidebar}>
+        <Sidebar />
+      </SidebarContainer>
+      <ContentContainer>
+        <Header />
+        <Outlet />
+      </ContentContainer>
+    </WebContainer>
+  );
+}
+
+const WebContainer = styled.div`
+  display: flex;
+  height: 100vh;
+`;
+
+const SidebarContainer = styled.div<SidebarContainerProps>`
+  width: ${({ toggle }) => (toggle ? "15rem" : "0")};
+  background-color: var(--gray100-color);
+  overflow-y: auto;
+  transition: width 270ms ease 0s;
+`;
+
+const ContentContainer = styled.div`
+  flex-grow: 1;
+  background-color: #fff;
+  overflow-y: auto;
+`;
+
+export default App;
---
+++ src/components/layouts/index.ts
@@ -0,0 +1,4 @@
+import Sidebar from "./sidebar/Sidebar";
+import Header from "./header/Header";
+
+export { Sidebar, Header };
