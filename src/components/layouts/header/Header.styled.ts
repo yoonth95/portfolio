a47ade318f9fe65af52af99008e8aae780af309d
윤태현 <yoonth0919@gmail.com>
2024-02-21 22:47:44 +0900

    Design: 헤더 마크업 및 디자인

--- src/components/layouts/header/Header.styled.ts
+++ src/components/layouts/header/Header.styled.ts
@@ -5,18 +5,47 @@ interface MenuButtonProps {
 }
 
 export const HeaderContainer = styled.header`
-  height: 45px;
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+  height: 3.75rem;
   padding: 0.75rem;
 `;
 
 export const MenuButton = styled.button<MenuButtonProps>`
+  display: ${({ toggle }) => (toggle ? "none" : "block")};
   width: 1.5rem;
   height: 1.5rem;
   transition: 270ms;
+  border-radius: 4px;
 
   & svg {
     color: rgba(55, 53, 47, 0.45);
-    opacity: ${({ toggle }) => (toggle ? 0 : 1)};
+  }
+
+  &:hover {
+    background-color: var(--gray300-color);
+  }
+`;
+
+export const Title = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+  transition: 270ms;
+  padding: 2px 5px;
+  border-radius: 4px;
+  cursor: pointer;
+
+  & p {
+    font-weight: 500;
+    font-size: 14px;
+    color: #000;
+  }
+
+  & img {
+    width: 1.5rem;
+    height: 1.5rem;
   }
 
   &:hover {
--- src/components/layouts/header/Header.tsx
+++ src/components/layouts/header/Header.tsx
@@ -1,21 +1,24 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
 import { useSidebarStore } from "@/stores";
+import notebook from "@/assets/images/notebook.png";
 import * as H from "./Header.styled";
 
 const Header = () => {
   const { toggleSidebar, setToggleSidebar } = useSidebarStore();
-
-  const openSidebar = () => {
-    setToggleSidebar();
-  };
+  const navigator = useNavigate();
 
   return (
     <H.HeaderContainer>
-      <H.MenuButton toggle={toggleSidebar} onClick={openSidebar}>
+      <H.MenuButton toggle={toggleSidebar} onClick={() => setToggleSidebar()}>
         <FontAwesomeIcon icon={faBars} />
       </H.MenuButton>
+      <H.Title onClick={() => navigator("/")}>
+        <img src={notebook} alt="노트북이미지" />
+        <p>yoonth0919</p>
+      </H.Title>
     </H.HeaderContainer>
   );
 };
