11045a7aaf262f4f11d6c9a4215929ddd31373e1
윤태현 <yoonth0919@gmail.com>
2024-02-21 23:57:51 +0900

    Add: 이미지 icon 추가

---
+++ src/assets/images/index.ts
@@ -0,0 +1,7 @@
+import notebook from "@/assets/images/notebook.png";
+import profile from "@/assets/images/profile.png";
+import book from "@/assets/images/book.png";
+import career from "@/assets/images/career.png";
+import trophy from "@/assets/images/trophy.png";
+
+export { profile, book, career, trophy, notebook };
