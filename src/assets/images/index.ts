e3fd664e1566660b5c77ba5e6b997a234b4da429
윤태현 <yoonth0919@gmail.com>
2024-02-23 00:35:32 +0900

    Add: 사이트 아이콘 추가

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -3,5 +3,9 @@ import profile from "@/assets/images/profile.png";
 import book from "@/assets/images/book.png";
 import career from "@/assets/images/career.png";
 import trophy from "@/assets/images/trophy.png";
+import beta from "@/assets/images/beta-beta-icon.png";
+import egg from "@/assets/images/egg-mbti-icon.png";
+import happymanwon from "@/assets/images/happymanwon-icon.png";
+import webBuilder from "@/assets/images/web-builder-icon.png";
 
-export { profile, book, career, trophy, notebook };
+export { profile, book, career, trophy, notebook, beta, egg, happymanwon, webBuilder };
