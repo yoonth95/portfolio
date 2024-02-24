649e43335081bcb2be2936ac3bea70340ad14b5f
윤태현 <yoonth0919@gmail.com>
2024-02-24 23:41:02 +0900

    Refactor: 데이터 파일 추가 및 코드 수정

--- src/components/layouts/sidebar/Sidebar.tsx
+++ src/components/layouts/sidebar/Sidebar.tsx
@@ -3,27 +3,8 @@ import { useLocation, useNavigate } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faAnglesLeft, faCaretRight } from "@fortawesome/free-solid-svg-icons";
 import { useSidebarStore } from "@/stores";
-import { profile, book, career, trophy, beta, egg, happymanwon, webBuilder } from "@/assets/images";
+import { tabList, projectList } from "@/data/sidebarInfo";
 import * as S from "./Sidebar.styled";
-
-const tabList = [
-  { icon: profile, text: "프로필", link: "/" },
-  { icon: book, text: "스킬", link: "/skill" },
-  { icon: career, text: "이력", link: "/career" },
-  { icon: trophy, text: "수상 및 자격증", link: "/award" },
-];
-
-const projectList = [
-  { title: "개인 프로젝트", projects: [{ icon: egg, text: "계란 요리 성격 유형 검사", link: "/project/egg-mbti" }] },
-  {
-    title: "팀 프로젝트",
-    projects: [
-      { icon: happymanwon, text: "만원의 행복", link: "/project/happymanwon" },
-      { icon: beta, text: "대학생 공연/전시 정보 플랫폼", link: "/project/beta-beta" },
-      { icon: webBuilder, text: "웅진씽크빅 웹 빌더 시스템", link: "/project/web-builder" },
-    ],
-  },
-];
 
 const Sidebar = () => {
   const navigator = useNavigate();
---
+++ src/data/sidebarInfo.ts
@@ -0,0 +1,22 @@
+import { profile, book, career, trophy, beta, egg, happymanwon, webBuilder } from "@/assets/images";
+
+const tabList = [
+  { icon: profile, text: "프로필", link: "/" },
+  { icon: book, text: "스킬", link: "/skill" },
+  { icon: career, text: "이력", link: "/career" },
+  { icon: trophy, text: "수상 및 자격증", link: "/award" },
+];
+
+const projectList = [
+  { title: "개인 프로젝트", projects: [{ icon: egg, text: "계란 요리 성격 유형 검사", link: "/project/egg-mbti" }] },
+  {
+    title: "팀 프로젝트",
+    projects: [
+      { icon: happymanwon, text: "만원의 행복", link: "/project/happymanwon" },
+      { icon: beta, text: "대학생 공연/전시 정보 플랫폼", link: "/project/beta-beta" },
+      { icon: webBuilder, text: "웅진씽크빅 웹 빌더 시스템", link: "/project/web-builder" },
+    ],
+  },
+];
+
+export { tabList, projectList };
