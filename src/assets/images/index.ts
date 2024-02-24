5f96929c0cc00a7a5377e31cc8118b203fd32db3
윤태현 <yoonth0919@gmail.com>
2024-02-24 23:39:40 +0900

    Add: 아이콘 추가

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -7,5 +7,44 @@ import beta from "@/assets/images/beta-beta-icon.png";
 import egg from "@/assets/images/egg-mbti-icon.png";
 import happymanwon from "@/assets/images/happymanwon-icon.png";
 import webBuilder from "@/assets/images/web-builder-icon.png";
+import profileImg from "@/assets/images/profileImg.png";
+import instagram from "@/assets/images/instagram.png";
+import velog from "@/assets/images/velog.png";
+import github from "@/assets/images/github.png";
+import gmail from "@/assets/images/gmail.png";
 
-export { profile, book, career, trophy, notebook, beta, egg, happymanwon, webBuilder };
+import aws from "@/assets/images/aws.png";
+import firebase from "@/assets/images/firebase.png";
+import flask from "@/assets/images/flask.png";
+import githubActions from "@/assets/images/github-actions.png";
+import javascript from "@/assets/images/javascript.png";
+import mysql from "@/assets/images/mysql.png";
+import nodejs from "@/assets/images/nodejs.png";
+import react from "@/assets/images/react.png";
+import typescript from "@/assets/images/typescript.png";
+
+export {
+  profile,
+  book,
+  career,
+  trophy,
+  notebook,
+  beta,
+  egg,
+  happymanwon,
+  webBuilder,
+  profileImg,
+  instagram,
+  velog,
+  github,
+  gmail,
+  aws,
+  firebase,
+  flask,
+  githubActions,
+  javascript,
+  mysql,
+  nodejs,
+  react,
+  typescript,
+};
