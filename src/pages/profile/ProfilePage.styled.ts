2479d20ac00b658e0a462306e29fb4cd84d47718
윤태현 <yoonth0919@gmail.com>
2024-02-27 00:25:02 +0900

    Design: 프로필 페이지 마크업 수정

--- src/pages/profile/ProfilePage.styled.ts
+++ src/pages/profile/ProfilePage.styled.ts
@@ -2,22 +2,15 @@ import styled from "styled-components";
 
 export const ProfileContanier = styled.main`
   padding: 3rem 5rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
   flex: 1;
-`;
-
-export const ProfileTitle = styled.h1`
-  font-size: 2rem;
-  font-weight: 600;
-  margin-bottom: 3rem;
 `;
 
 export const ProfileSection = styled.div`
   display: flex;
   justify-content: center;
   gap: 4rem;
-
-  & > section {
-    display: flex;
-    flex-direction: column;
-  }
+  height: 600px;
 `;
