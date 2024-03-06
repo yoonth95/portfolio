2c17389bc17be70546e1dfbd5f38ca5c2a485773
윤태현 <yoonth0919@gmail.com>
2024-03-06 23:42:44 +0900

    Feat: Slick 컴포넌트 이미지 지연 로딩 적용

--- src/components/slick/Slick.tsx
+++ src/components/slick/Slick.tsx
@@ -1,14 +1,19 @@
 import React from "react";
-import Slider from "react-slick";
+import Slider, { Settings } from "react-slick";
 import { SlickProps } from "@/types";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import * as S from "./Slick.styled";
 
 const Slick: React.FC<SlickProps> = ({ slickSetting, children }) => {
+  const settings: Settings = {
+    lazyLoad: "anticipated",
+    ...slickSetting,
+  };
+
   return (
     <S.SlickContainer>
-      <Slider {...slickSetting}>{children}</Slider>
+      <Slider {...settings}>{children}</Slider>
     </S.SlickContainer>
   );
 };
