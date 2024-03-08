dd2a7b94fcc5c214ade10df60367a0b4d9cf6a7c
윤태현 <yoonth0919@gmail.com>
2024-03-08 22:51:41 +0900

    Design: 이력 페이지 경력 컴포넌트 마크업 및 디자인

--- src/components/career/careerExp/CareerExp.styled.ts
+++ src/components/career/careerExp/CareerExp.styled.ts
@@ -1,1 +1,24 @@
 import styled from "styled-components";
+
+export const DescriptionList = styled.div`
+  padding: 1rem 0;
+`;
+
+export const DescriptionLink = styled.a`
+  font-style: italic;
+  color: #525252bf;
+
+  &:hover {
+    text-decoration: underline;
+  }
+`;
+
+export const DescriptionUl = styled.ul`
+  margin-top: 15px;
+  & li {
+    font-size: 15px;
+    line-height: 1.5;
+    list-style: outside;
+    margin: 0 0 0.625rem 1.5rem;
+  }
+`;
--- src/components/career/careerExp/CareerExp.tsx
+++ src/components/career/careerExp/CareerExp.tsx
@@ -7,7 +7,34 @@ interface CareerExpProps {
 }
 
 const CareerExp: React.FC<CareerExpProps> = ({ data }) => {
-  return <div></div>;
+  return (
+    <>
+      <h2>{data.title}</h2>
+      <h3>{`${data.subTitle}`}</h3>
+      <ul className="techStackList">
+        {data.techStack?.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
+      </ul>
+      {data.description?.map((item, index) => (
+        <C.DescriptionList key={index}>
+          <div className="quotation">
+            <strong>{item.title}</strong>
+            <span>{item.duration}</span>
+          </div>
+
+          <C.DescriptionLink href={item.link} target="_blank">
+            {item.link}
+          </C.DescriptionLink>
+          <C.DescriptionUl>
+            {item.works.map((work, idx) => (
+              <li key={idx}>{work}</li>
+            ))}
+          </C.DescriptionUl>
+        </C.DescriptionList>
+      ))}
+    </>
+  );
 };
 
 export default CareerExp;
