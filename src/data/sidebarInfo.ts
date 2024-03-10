917d817ff278976c3043f92c30a8c94038291dd0
윤태현 <yoonth0919@gmail.com>
2024-03-11 00:13:55 +0900

    Add: 프로젝트 데이터 추가

--- src/data/sidebarInfo.ts
+++ src/data/sidebarInfo.ts
@@ -1,4 +1,4 @@
-import { profile, career, trophy, beta, egg, happymanwon, webBuilder } from "@/assets/images";
+import { profile, career, trophy, beta, egg, eggMbti, happymanwon, webBuilder } from "@/assets/images";
 
 const tabList = [
   { icon: profile, text: "프로필", link: "/" },
@@ -7,13 +7,29 @@ const tabList = [
 ];
 
 const projectList = [
-  { title: "개인 프로젝트", projects: [{ icon: egg, text: "계란 요리 성격 유형 검사", link: "/project/egg-mbti" }] },
+  {
+    title: "개인 프로젝트",
+    projects: [
+      {
+        thumbnail: eggMbti,
+        date: "2023-09 ~ 2023-10",
+        techStack: [
+          ["React", "#d3e5ef"],
+          ["Styled-Components", "#dbeddb"],
+          ["AWS", "#ffe2dd"],
+        ],
+        icon: egg,
+        text: "계란 요리 성격 유형 검사",
+        link: "/project/egg-mbti",
+      },
+    ],
+  },
   {
     title: "팀 프로젝트",
     projects: [
-      { icon: happymanwon, text: "만원의 행복", link: "/project/happymanwon" },
-      { icon: beta, text: "대학생 공연/전시 정보 플랫폼", link: "/project/beta-beta" },
-      { icon: webBuilder, text: "웅진씽크빅 웹 빌더 시스템", link: "/project/web-builder" },
+      { thumbnail: "", date: "", techStack: [], icon: happymanwon, text: "만원의 행복", link: "/project/happymanwon" },
+      { thumbnail: "", date: "", techStack: [], icon: beta, text: "대학생 공연/전시 정보 플랫폼", link: "/project/beta-beta" },
+      { thumbnail: "", date: "", techStack: [], icon: webBuilder, text: "웅진씽크빅 웹 빌더 시스템", link: "/project/web-builder" },
     ],
   },
 ];
