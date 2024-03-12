6db5daad9fbaa16d1755e36654c05344523296c4
윤태현 <yoonth0919@gmail.com>
2024-03-12 22:28:40 +0900

    Feat: 프로젝트 페이지 모달에 프로젝트 동적 할당

--- src/pages/project/ProjectPage.tsx
+++ src/pages/project/ProjectPage.tsx
@@ -1,28 +1,39 @@
 import { ProjectBox } from "@/components/project";
-import { projectList } from "@/data/sidebarInfo";
+import { LazyImage } from "@/components/common";
+import { Modal } from "@/components/layouts";
+import { BetaBetaPage, EggMbtiPage, HappymanwonPage, WebBuilderPage } from "@/pages";
+import { useModalStore } from "@/stores";
+import { projectList } from "@/data/projectList";
 import { desktopGif } from "@/assets/images";
 import * as P from "./ProjectPage.styled";
 
 const ProjectPage = () => {
+  const { isOpen, dataName } = useModalStore();
+
+  const pages = {
+    "egg-mbti": <EggMbtiPage />,
+    happymanwon: <HappymanwonPage />,
+    "beta-beta": <BetaBetaPage />,
+    "web-builder": <WebBuilderPage />,
+  };
+
   return (
     <P.ProjectContainer>
       <section>
         <P.ProjectTitle>
-          <img src={desktopGif} alt="트로피gif" />
+          <LazyImage src={desktopGif} alt={"트로피gif"} />
           <h2>프로젝트</h2>
         </P.ProjectTitle>
         <hr />
         <P.ProjectBoxList>
-          {projectList
-            .map((item) => item.projects)
-            .flat()
-            .map((item, index) => (
-              <li key={index}>
-                <ProjectBox data={item} />
-              </li>
-            ))}
+          {projectList.map((item) => (
+            <li key={item.name}>
+              <ProjectBox data={item} />
+            </li>
+          ))}
         </P.ProjectBoxList>
       </section>
+      {isOpen && <Modal>{pages[dataName as keyof typeof pages]}</Modal>}
     </P.ProjectContainer>
   );
 };
