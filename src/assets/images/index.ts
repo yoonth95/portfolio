3ac1ddded5f5b0f23815c14d2eaf20dda2e80748
윤태현 <yoonth0919@gmail.com>
2024-03-16 00:22:15 +0900

    Move: img 폴더 구조 변경

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,35 +1,34 @@
-import notebook from "@/assets/images/notebook.png";
-import profile from "@/assets/images/profile.png";
-import book from "@/assets/images/book.png";
-import career from "@/assets/images/career.png";
-import trophy from "@/assets/images/trophy.png";
-import beta from "@/assets/images/beta-beta-icon.png";
-import betaTumbnail from "@/assets/images/beta-thumbnail.png";
-import egg from "@/assets/images/egg-mbti-icon.png";
+import aws from "@/assets/images/icons/aws.png";
+import firebase from "@/assets/images/icons/firebase.png";
+import flask from "@/assets/images/icons/flask.png";
+import githubActions from "@/assets/images/icons/github-actions.png";
+import javascript from "@/assets/images/icons/javascript.png";
+import mysql from "@/assets/images/icons/mysql.png";
+import nodejs from "@/assets/images/icons/nodejs.png";
+import react from "@/assets/images/icons/react.png";
+import typescript from "@/assets/images/icons/typescript.png";
+import notebook from "@/assets/images/icons/notebook.png";
+import profile from "@/assets/images/icons/profile.png";
+import book from "@/assets/images/icons/book.png";
+import career from "@/assets/images/icons/career.png";
+import trophy from "@/assets/images/icons/trophy.png";
+import beta from "@/assets/images/icons/beta-beta-icon.png";
+import egg from "@/assets/images/icons/egg-mbti-icon.png";
+import happymanwon from "@/assets/images/icons/happymanwon-icon.png";
+import webBuilder from "@/assets/images/icons/web-builder-icon.png";
+import instagram from "@/assets/images/icons/instagram.png";
+import velog from "@/assets/images/icons/velog.png";
+import github from "@/assets/images/icons/github.png";
+import gmail from "@/assets/images/icons/gmail.png";
+import certifivation from "@/assets/images/icons/certification.gif";
+import trophyGif from "@/assets/images/icons/trophy.gif";
+import desktopGif from "@/assets/images/icons/desktop.gif";
+
 import eggMbti from "@/assets/images/egg-mbti.png";
-import happymanwon from "@/assets/images/happymanwon-icon.png";
 import happymanwonThumbnail from "@/assets/images/happymanwon.png";
-import webBuilder from "@/assets/images/web-builder-icon.png";
 import webBuilderThumbnail from "@/assets/images/web-builder-thumbnail.png";
+import betaTumbnail from "@/assets/images/beta-thumbnail.png";
 import profileImg from "@/assets/images/profileImg.png";
-import instagram from "@/assets/images/instagram.png";
-import velog from "@/assets/images/velog.png";
-import github from "@/assets/images/github.png";
-import gmail from "@/assets/images/gmail.png";
-import certifivation from "@/assets/images/certification.gif";
-import trophyGif from "@/assets/images/trophy.gif";
-
-import desktopGif from "@/assets/images/desktop.gif";
-
-import aws from "@/assets/images/aws.png";
-import firebase from "@/assets/images/firebase.png";
-import flask from "@/assets/images/flask.png";
-import githubActions from "@/assets/images/github-actions.png";
-import javascript from "@/assets/images/javascript.png";
-import mysql from "@/assets/images/mysql.png";
-import nodejs from "@/assets/images/nodejs.png";
-import react from "@/assets/images/react.png";
-import typescript from "@/assets/images/typescript.png";
 
 export {
   profile,
