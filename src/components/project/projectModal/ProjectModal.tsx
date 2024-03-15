fed5c4007d2226a77e70f006a1e03ef12f5bfdae
윤태현 <yoonth0919@gmail.com>
2024-03-16 00:54:40 +0900

    Feat: 프로젝트 모달 이전 다음 버튼 조건부 로직 적용

--- src/components/project/projectModal/ProjectModal.tsx
+++ src/components/project/projectModal/ProjectModal.tsx
@@ -1,10 +1,11 @@
 import React from "react";
+import { createPortal } from "react-dom";
 import { useNavigate } from "react-router-dom";
 import { useModalStore } from "@/stores";
+import { projectList } from "@/data/projectList";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faAngleDown, faAngleUp, faUpRightAndDownLeftFromCenter, faXmark } from "@fortawesome/free-solid-svg-icons";
 import * as M from "./ProjectModal.styled";
-import { createPortal } from "react-dom";
 
 interface ProjectModalProps {
   children: React.ReactNode;
@@ -20,16 +21,23 @@ const ProjectModal: React.FC<ProjectModalProps> = ({ children }) => {
   };
 
   const movePrev = () => {
-    setProjectId(1);
+    projectId > 1 && setProjectId(projectId - 1);
   };
 
   const moveNext = () => {
-    setProjectId(2);
+    projectId < projectList.length && setProjectId(projectId + 1);
   };
 
   const closeModal = () => {
     setIsOpen(false);
     setProjectId(0);
+  };
+
+  const calculateStyle = (isActive: boolean) => {
+    return {
+      color: isActive ? "#37352f73" : "#37352f29",
+      cursor: isActive ? "pointer" : "default",
+    };
   };
 
   const renderModal = (
@@ -41,10 +49,10 @@ const ProjectModal: React.FC<ProjectModalProps> = ({ children }) => {
               <FontAwesomeIcon icon={faUpRightAndDownLeftFromCenter} style={{ transform: "rotate(90deg)" }} />
             </M.ModalHeaderIcon>
             <M.ModalHeaderLine></M.ModalHeaderLine>
-            <M.ModalHeaderIcon onClick={movePrev}>
+            <M.ModalHeaderIcon onClick={movePrev} style={calculateStyle(projectId > 1)}>
               <FontAwesomeIcon icon={faAngleUp} />
             </M.ModalHeaderIcon>
-            <M.ModalHeaderIcon onClick={moveNext}>
+            <M.ModalHeaderIcon onClick={moveNext} style={calculateStyle(projectId < projectList.length)}>
               <FontAwesomeIcon icon={faAngleDown} />
             </M.ModalHeaderIcon>
           </M.ModalHeaderLeft>
