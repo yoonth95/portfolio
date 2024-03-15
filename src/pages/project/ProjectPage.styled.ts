1b5da26b26051b13f32bb9d61b4d42d472821a48
윤태현 <yoonth0919@gmail.com>
2024-03-16 00:28:58 +0900

    Design: 프로젝트 페이지 마크업 및 디자인

--- src/pages/project/ProjectPage.styled.ts
+++ src/pages/project/ProjectPage.styled.ts
@@ -1,28 +1,9 @@
 import styled from "styled-components";
 
-export const ProjectContainer = styled.main`
+export const ProjectContainer = styled.div`
+  height: 100%;
   display: flex;
   flex-direction: column;
-  flex: 1;
-  gap: 5rem;
-  padding: 3rem 13rem;
-`;
-
-export const ProjectTitle = styled.div`
-  display: flex;
-  align-items: center;
-  gap: 10px;
-
-  & h2 {
-    font-size: 20px;
-    margin: 0;
-  }
-`;
-
-export const ProjectBoxList = styled.ul`
-  padding-top: 10px;
-  display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(320px, 1fr));
-  gap: 2.25rem;
-  width: 100%;
+  margin: 0 10rem;
+  /* overflow-y: auto; */
 `;
--- src/pages/project/ProjectPage.tsx
+++ src/pages/project/ProjectPage.tsx
@@ -1,39 +1,19 @@
-import { ProjectBox } from "@/components/project";
-import { LazyImage } from "@/components/common";
-import { Modal } from "@/components/layouts";
-import { BetaBetaPage, EggMbtiPage, HappymanwonPage, WebBuilderPage } from "@/pages";
-import { useModalStore } from "@/stores";
-import { projectList } from "@/data/projectList";
-import { desktopGif } from "@/assets/images";
+import React from "react";
+import { ProjectHeader } from "@/components/project";
+import { useWithProject } from "@/hooks";
 import * as P from "./ProjectPage.styled";
 
 const ProjectPage = () => {
-  const { isOpen, dataName } = useModalStore();
+  const { isModal, ...projectData } = useWithProject();
 
-  const pages = {
-    "egg-mbti": <EggMbtiPage />,
-    happymanwon: <HappymanwonPage />,
-    "beta-beta": <BetaBetaPage />,
-    "web-builder": <WebBuilderPage />,
-  };
+  if (!projectData) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <P.ProjectContainer>
-      <section>
-        <P.ProjectTitle>
-          <LazyImage src={desktopGif} alt={"트로피gif"} />
-          <h2>프로젝트</h2>
-        </P.ProjectTitle>
-        <hr />
-        <P.ProjectBoxList>
-          {projectList.map((item) => (
-            <li key={item.name}>
-              <ProjectBox data={item} />
-            </li>
-          ))}
-        </P.ProjectBoxList>
-      </section>
-      {isOpen && <Modal>{pages[dataName as keyof typeof pages]}</Modal>}
+      <ProjectHeader projectData={projectData} isModal={isModal} />
+      <hr />
     </P.ProjectContainer>
   );
 };
