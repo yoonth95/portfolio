7421b53b25679bd44fbf597342f0e33b8615aef8
윤태현 <yoonth0919@gmail.com>
2024-03-16 17:30:20 +0900

    Refactor: 사이드바 프로젝트 데이터 이동 링크 수정

--- src/data/sidebarInfo.ts
+++ src/data/sidebarInfo.ts
@@ -7,13 +7,13 @@ const tabList = [
 ];
 
 const projectTabList = [
-  { type: "개인 프로젝트", projects: [{ icon: egg, title: "계란 요리 성격 유형 검사", link: "/project/egg-mbti" }] },
+  { type: "개인 프로젝트", projects: [{ icon: egg, title: "계란 요리 성격 유형 검사", link: "/project/1" }] },
   {
     type: "팀 프로젝트",
     projects: [
-      { icon: happymanwon, title: "만원의 행복", link: "/project/happymanwon" },
-      { icon: beta, title: "대학생 공연/전시 정보 플랫폼", link: "/project/beta-beta" },
-      { icon: webBuilder, title: "웅진씽크빅 웹 빌더 시스템", link: "/project/web-builder" },
+      { icon: happymanwon, title: "만원의 행복", link: "/project/2" },
+      { icon: beta, title: "대학생 공연/전시 정보 플랫폼", link: "/project/3" },
+      { icon: webBuilder, title: "웅진씽크빅 웹 빌더 시스템", link: "/project/4" },
     ],
   },
 ];
