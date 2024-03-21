9afe54b38e234189c12b63b95db079cdc5c9b083
윤태현 <yoonth0919@gmail.com>
2024-03-21 16:00:06 +0900

    Feat: react-scroll 기능 추가

--- src/components/project/projectNav/ProjectNav.tsx
+++ src/components/project/projectNav/ProjectNav.tsx
@@ -1,20 +1,41 @@
-import React from "react";
-import { useProjectNavStore } from "@/stores";
+import React, { useEffect, useState } from "react";
+import { useSidebarStore } from "@/stores";
 import * as PN from "./ProjectNav.styled";
+import { Link } from "react-scroll";
 
 interface ProjectNavProps {
   isModal: boolean;
   headings: string[];
 }
 
+const duration = 500;
+
 const ProjectNav: React.FC<ProjectNavProps> = ({ isModal, headings }) => {
-  const { isOpen } = useProjectNavStore();
+  const { toggleSidebar } = useSidebarStore();
+  const [isOpen, setIsOpen] = useState(true);
+
+  useEffect(() => {
+    const resizeWidth = () => {
+      if (toggleSidebar) {
+        window.innerWidth < 1540 ? setIsOpen(false) : setIsOpen(true);
+      } else {
+        window.innerWidth < 1300 ? setIsOpen(false) : setIsOpen(true);
+      }
+    };
+
+    resizeWidth();
+    window.addEventListener("resize", resizeWidth);
+
+    return () => window.addEventListener("resize", resizeWidth);
+  }, [toggleSidebar]);
 
   return (
     <PN.Navbar $isOpen={isOpen} $isModal={isModal}>
       <PN.ProjectNav>
-        {headings.map((item) => (
-          <li key={item}>{item}</li>
+        {headings.map((item, index) => (
+          <Link key={index} activeClass="active" to={`section${index + 1}`} spy smooth duration={duration} offset={-60}>
+            <li>{item}</li>
+          </Link>
         ))}
       </PN.ProjectNav>
     </PN.Navbar>
