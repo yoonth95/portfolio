53758c3c6898da42235b3a83850e804707f0f92e
윤태현 <yoonth0919@gmail.com>
2024-03-21 17:52:15 +0900

    Feat: 프로젝트 별 내용 분할

--- src/pages/project/ProjectPage.tsx
+++ src/pages/project/ProjectPage.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useRef, useState } from "react";
+import { useLocation } from "react-router-dom";
 import { Element } from "react-scroll";
 import { ProjectHeader, ProjectLinkBox, ProjectNav } from "@/components/project";
 import { useWithProject } from "@/hooks";
@@ -8,14 +9,15 @@ const ProjectPage = () => {
   const { isModal, ...projectData } = useWithProject();
   const [headings, setHeadings] = useState<string[]>([]);
   const containerRef = useRef<HTMLDivElement>(null);
+  const { pathname } = useLocation();
 
   useEffect(() => {
     if (containerRef.current) {
-      const h1Elements = containerRef.current.querySelectorAll("h1");
-      const headingsList = Array.from(h1Elements).map((h1) => h1.textContent || "");
+      const elements = containerRef.current.querySelectorAll("h1, h2");
+      const headingsList = Array.from(elements).map((el) => `${el.tagName}_${el.textContent}` || "");
       setHeadings(headingsList);
     }
-  }, []);
+  }, [pathname]);
 
   if (!projectData) {
     return <div>Loading...</div>;
@@ -29,28 +31,24 @@ const ProjectPage = () => {
       <ProjectNav isModal={isModal} headings={headings} />
       <P.ProjectInfo ref={containerRef}>
         <Element name="section1">
-          <P.H1>배포 사이트</P.H1>
+          <h1>배포 사이트</h1>
           <ProjectLinkBox projectData={projectData} />
         </Element>
 
         <Element name="section2">
-          {/* <P.H1>배포 사이트</P.H1> */}
-          {/* <ProjectLinkBox projectData={projectData} /> */}
-        </Element>
-
-        <Element name="section3">
-          {/* <P.H1>배포 사이트</P.H1> */}
-          {/* <ProjectLinkBox projectData={projectData} /> */}
-        </Element>
-
-        <Element name="section4">
-          {/* <P.H1>배포 사이트</P.H1> */}
-          {/* <ProjectLinkBox projectData={projectData} /> */}
-        </Element>
-
-        <Element name="section5">
-          {/* <P.H1>배포 사이트</P.H1> */}
-          {/* <ProjectLinkBox projectData={projectData} /> */}
+          <h1>프로젝트 소개</h1>
+          {projectData.intention && (
+            <Element name="section3">
+              <h2>기획 의도</h2>
+              <p>{projectData.intention}</p>
+            </Element>
+          )}
+          {projectData.architecture && (
+            <Element name="section4">
+              <h2>프로젝트 아키텍쳐</h2>
+              <P.ArchitectureImg src={projectData.architecture} alt="프로젝트 아키텍쳐" />
+            </Element>
+          )}
         </Element>
       </P.ProjectInfo>
     </P.ProjectContainer>
