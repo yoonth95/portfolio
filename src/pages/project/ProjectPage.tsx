b2217852a84c264d0f02d7e8b2056ecc42d36912
윤태현 <yoonth0919@gmail.com>
2024-03-22 17:56:41 +0900

    Design: 프로젝트 페이지 내용 부분 마크업

--- src/pages/project/ProjectPage.tsx
+++ src/pages/project/ProjectPage.tsx
@@ -30,25 +30,48 @@ const ProjectPage = () => {
       <br />
       <ProjectNav isModal={isModal} headings={headings} />
       <P.ProjectInfo ref={containerRef}>
-        <Element name="section1">
-          <h1>배포 사이트</h1>
-          <ProjectLinkBox projectData={projectData} />
-        </Element>
-
-        <Element name="section2">
+        <Element className="section" name="section1">
           <h1>프로젝트 소개</h1>
           {projectData.intention && (
-            <Element name="section3">
+            <Element className="section" name="section2">
               <h2>기획 의도</h2>
               <p>{projectData.intention}</p>
             </Element>
           )}
           {projectData.architecture && (
-            <Element name="section4">
+            <Element className="section" name="section3">
               <h2>프로젝트 아키텍쳐</h2>
-              <P.ArchitectureImg src={projectData.architecture} alt="프로젝트 아키텍쳐" />
+              <img src={projectData.architecture} alt="프로젝트 아키텍쳐" />
             </Element>
           )}
+          <Element className="section" name="section4">
+            <h2>기술 선정 이유</h2>
+            {/* <P.ArchitectureImg src={projectData.architecture} alt="프로젝트 아키텍쳐" /> */}
+          </Element>
+          <Element className="section" name="section5">
+            <h2>피그마/디자인</h2>
+            {/* <P.ArchitectureImg src={projectData.architecture} alt="프로젝트 아키텍쳐" /> */}
+          </Element>
+          <Element className="section" name="section6">
+            <h2>API / ERP 설계</h2>
+            {/* <P.ArchitectureImg src={projectData.architecture} alt="프로젝트 아키텍쳐" /> */}
+          </Element>
+        </Element>
+        <Element className="section" name="section7">
+          <h1>담당한 기능</h1>
+          {/* <ProjectLinkBox projectData={projectData} /> */}
+        </Element>
+        <Element className="section" name="section8">
+          <h1>트러블슈팅</h1>
+          {/* <ProjectLinkBox projectData={projectData} /> */}
+        </Element>
+        <Element className="section" name="section9">
+          <h1>상세 이미지</h1>
+          {/* <ProjectLinkBox projectData={projectData} /> */}
+        </Element>
+        <Element className="section" name="section10">
+          <h1>배포 사이트</h1>
+          <ProjectLinkBox projectData={projectData} />
         </Element>
       </P.ProjectInfo>
     </P.ProjectContainer>
