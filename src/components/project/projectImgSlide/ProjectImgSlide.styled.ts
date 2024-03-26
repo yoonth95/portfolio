0b109fbef6e8ba69372b0c3ffacbfbcd54bbd879
윤태현 <yoonth0919@gmail.com>
2024-03-26 22:46:32 +0900

    Design: 프로젝트 이미지 슬라이더 컴포넌트 prev, next 버튼 z-index 추가

--- src/components/project/projectImgSlide/ProjectImgSlide.styled.ts
+++ src/components/project/projectImgSlide/ProjectImgSlide.styled.ts
@@ -6,9 +6,11 @@ interface SlideContainerProps {
 export const SlideContainer = styled.div<SlideContainerProps>`
   .slick-prev {
     left: 0px;
+    z-index: 1;
   }
   .slick-next {
     right: 0px;
+    z-index: 1;
   }
   .slick-prev:before,
   .slick-next:before {
