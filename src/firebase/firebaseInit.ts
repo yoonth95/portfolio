a0fb5e7afe3ce139d31f9fbae2eb5bb27c6757c5
윤태현 <yoonth0919@gmail.com>
2024-03-30 19:47:54 +0900

    Refactor: firebase 설정 변경

--- src/firebase/firebaseInit.ts
+++ src/firebase/firebaseInit.ts
@@ -1,23 +1,21 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const { VITE_APP_APIKEY, VITE_APP_AUTHDOMAIN, VITE_APP_PROJECTID, VITE_APP_STORAGEBUCKET, VITE_APP_MESSAGINGSENDERID, VITE_APP_APPID } = import.meta
+  .env;
+
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_APP_APIKEY,
-  authDomain: import.meta.env.VITE_APP_AUTHDOMAIN,
-  projectId: import.meta.env.VITE_APP_PROJECTID,
-  storageBucket: import.meta.env.VITE_APP_STORAGEBUCKET,
-  messagingSenderId: import.meta.env.VITE_APP_MESSAGINGSENDERID,
-  appId: import.meta.env.VITE_APP_APPID,
-  measurementId: import.meta.env.VITE_APP_MEASUREMENTID,
+  apiKey: VITE_APP_APIKEY,
+  authDomain: VITE_APP_AUTHDOMAIN,
+  projectId: VITE_APP_PROJECTID,
+  storageBucket: VITE_APP_STORAGEBUCKET,
+  messagingSenderId: VITE_APP_MESSAGINGSENDERID,
+  appId: VITE_APP_APPID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
-export { analytics };
+export { app, analytics };
