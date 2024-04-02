f98031b29b5ca853f2ae42c897c33ebec2bb1e1b
윤태현 <yoonth0919@gmail.com>
2024-04-02 16:41:08 +0900

    Design: 프로젝트 페이지 반응형 디자인 추가

--- src/pages/projectList/ProjectListPage.styled.ts
+++ src/pages/projectList/ProjectListPage.styled.ts
@@ -6,6 +6,22 @@ export const ProjectListContainer = styled.div`
   flex: 1;
   gap: 5rem;
   padding: 3rem 13rem;
+
+  @media (max-width: 1025px) {
+    padding: 3rem 3rem;
+  }
+
+  @media (max-width: 769px) {
+    padding: 3rem 2rem;
+  }
+
+  @media (max-width: 376px) {
+    padding: 3rem 1rem;
+  }
+
+  @media (max-width: 321px) {
+    padding: 1rem 0.5rem;
+  }
 `;
 
 export const ProjectListTitle = styled.div`
@@ -17,6 +33,18 @@ export const ProjectListTitle = styled.div`
     font-size: 20px;
     margin: 0;
   }
+
+  @media (max-width: 321px) {
+    & img {
+      width: 36px;
+      height: 36px;
+    }
+
+    & h2 {
+      font-size: 15px;
+      margin: 0;
+    }
+  }
 `;
 
 export const ProjectListArea = styled.ul`
@@ -25,4 +53,8 @@ export const ProjectListArea = styled.ul`
   grid-template-columns: repeat(auto-fill, minmax(320px, 1fr));
   gap: 2.25rem;
   width: 100%;
+
+  @media (max-width: 400px) {
+    grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+  }
 `;
