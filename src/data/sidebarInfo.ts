b71f340d84058dd94f5780391740e3e3cbf6edbf
윤태현 <78673090+yoonth95@users.noreply.github.com>
2024-08-24 01:28:04 +0900

    refactor: 사이드바 데이터 추가로 인한 스타일 수정

--- src/data/sidebarInfo.ts
+++ src/data/sidebarInfo.ts
@@ -7,14 +7,15 @@ const tabList = [
 ];
 
 const projectTabList = [
-  { type: "개인 프로젝트", projects: [{ icon: egg, title: "[EBTI] 계란 요리 성격 유형 검사", link: "/project/1" }] },
+  { type: "개인 프로젝트", projects: [{ icon: egg, title: "[EBTI] 계란 요리 성격 유형 검사", link: "/project/egg-mbti" }] },
   {
     type: "팀 프로젝트",
     projects: [
-      { icon: beta, title: "대학생 공연/전시 정보 플랫폼", link: "/project/2" },
-      { icon: happymanwon, title: "만원의 행복", link: "/project/3" },
-      { icon: webBuilder, title: "웅진씽크빅 웹 빌더 시스템", link: "/project/4" },
-      { icon: youtubeCloneIcon, title: "477Tube", link: "/project/5" },
+      { icon: beta, title: "해외 주식 분석 플랫폼", link: "/project/aightnow" },
+      { icon: beta, title: "대학생 공연/전시 정보 플랫폼", link: "/project/beta-beta" },
+      { icon: happymanwon, title: "만원의 행복", link: "/project/happymanwon" },
+      { icon: webBuilder, title: "웅진씽크빅 웹 빌더 시스템", link: "/project/web-builder" },
+      { icon: youtubeCloneIcon, title: "477Tube", link: "/project/youtube-clone" },
     ],
   },
 ];
