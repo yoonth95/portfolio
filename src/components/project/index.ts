effdedd3daaa3d3f4d50bb81db1d0b174f4f00b1
윤태현 <78673090+yoonth95@users.noreply.github.com>
2025-01-14 17:43:21 +0900

    ReName: ProjectImgRender to ProjectRender 폴더 이름 변경

--- src/components/project/index.ts
+++ src/components/project/index.ts
@@ -4,6 +4,7 @@ import ProjectModal from "./projectModal/ProjectModal";
 import ProjectLinkBox from "./projectLinkBox/ProjectLinkBox";
 import ProjectNav from "./projectNav/ProjectNav";
 import ProjectImgSlide from "./projectImgSlide/ProjectImgSlide";
-import ProjectImgRender from "./projectImgRender/ProjectImgRender";
+import ProjectImgRender from "./projectRender/ProjectImgRender";
+import ProjectPropulsionRender from "./projectRender/ProjectPropulsionRender";
 
-export { ProjectBox, ProjectHeader, ProjectModal, ProjectLinkBox, ProjectNav, ProjectImgSlide, ProjectImgRender };
+export { ProjectBox, ProjectHeader, ProjectModal, ProjectLinkBox, ProjectNav, ProjectImgSlide, ProjectImgRender, ProjectPropulsionRender };
