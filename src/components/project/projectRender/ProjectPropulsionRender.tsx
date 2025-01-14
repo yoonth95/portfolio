13a91e8a49eb6315051bd0708107b615d1a7f552
윤태현 <78673090+yoonth95@users.noreply.github.com>
2025-01-14 17:48:27 +0900

    Refactor: propulsionData 렌더 시 undefined 체크

--- src/components/project/projectRender/ProjectPropulsionRender.tsx
+++ src/components/project/projectRender/ProjectPropulsionRender.tsx
@@ -14,25 +14,29 @@ const ProjectPropulsionRender = ({ propulsionData }: { propulsionData: ProjectPr
 
         const aspectRatio = aspectRatioItem.aspectRatio || "16/9";
 
-        return Object.keys(contentItem).map((key) => (
-          <li
-            key={index}
-            style={{
-              marginBottom: index === array.length - 1 ? "3rem" : "5rem",
-            }}
-          >
-            <h3 id="sectionPropulsionDetail">{key}</h3>
-            {Array.isArray(contentItem[key]) ? (
-              <ProjectImgSlide aspectRatio={aspectRatio}>
-                {contentItem[key].map((imgSrc: string, imgIndex: number) => (
-                  <img key={imgIndex} src={imgSrc} alt={`이미지 ${imgIndex}`} />
-                ))}
-              </ProjectImgSlide>
-            ) : (
-              <img src={contentItem[key] as string} alt={`이미지`} style={{ width: "100%", aspectRatio, objectFit: "contain" }} />
-            )}
-          </li>
-        ));
+        return Object.keys(contentItem).map((key) => {
+          const value = contentItem[key];
+
+          return (
+            <li
+              key={index}
+              style={{
+                marginBottom: index === array.length - 1 ? "3rem" : "5rem",
+              }}
+            >
+              <h3 id="sectionPropulsionDetail">{key}</h3>
+              {Array.isArray(value) ? (
+                <ProjectImgSlide aspectRatio={aspectRatio}>
+                  {value.map((imgSrc: string, imgIndex: number) => (
+                    <img key={imgIndex} src={imgSrc} alt={`이미지 ${imgIndex}`} />
+                  ))}
+                </ProjectImgSlide>
+              ) : (
+                value && <img src={value as string} alt={`이미지`} style={{ width: "100%", aspectRatio, objectFit: "contain" }} />
+              )}
+            </li>
+          );
+        });
       })}
     </ul>
   );
